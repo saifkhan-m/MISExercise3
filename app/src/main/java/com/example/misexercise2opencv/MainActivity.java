8c4950baae4f8c07717e9e74fa390cdffb33d910
Saif Khan <Saif Khan>
2019-05-06 23:44:29 +0200

    Instantiated face detedtion and clown nose effect

--- app/src/main/java/com/example/misexercise2opencv/MainActivity.java
+++ app/src/main/java/com/example/misexercise2opencv/MainActivity.java
@@ -6,13 +6,18 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
 import android.Manifest;
 import android.app.Activity;
@@ -41,6 +46,9 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     private CameraBridgeViewBase    mOpenCvCameraView;
     private boolean                 mIsJavaCamera = true;
     private MenuItem                mItemSwitchCamera = null;
+    private CascadeClassifier cascadeClassifier;
+    private Mat grayscaleImage;
+    private int absoluteFaceSize;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -54,6 +62,7 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
                 default:
                 {
                     super.onManagerConnected(status);
+                    Log.i(TAG, "OpenCV NOt loaded succesfully");
                 } break;
             }
         }
@@ -112,11 +121,17 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     }
 
     public void onCameraViewStarted(int width, int height) {
+        grayscaleImage = new Mat(height, width, CvType.CV_8UC4);
+
+        // The faces will be a 20% of the height of the screen
+        absoluteFaceSize = (int) (height * 0.2);
     }
 
     public void onCameraViewStopped() {
     }
-
+//Inspiration from
+// https://docs.opencv.org/3.4.1/d7/d8b/tutorial_py_face_detection.html
+// https://www.mirkosertic.de/blog/2013/07/realtime-face-detection-on-android-using-opencv/
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
 
         //return inputFrame.rgba();
@@ -127,12 +142,41 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         return col;
         */
 
-        Mat gray = inputFrame.gray();
-        Mat col  = inputFrame.rgba();
 
+        Mat gray = inputFrame.gray();
+
+        Mat col  = inputFrame.rgba();
+        Core.rotate(gray.t(),gray,0);
         Mat tmp = gray.clone();
-        Imgproc.Canny(gray, tmp, 80, 100);
-        Imgproc.cvtColor(tmp, col, Imgproc.COLOR_GRAY2RGBA, 4);
+        //Imgproc.Canny(gray, tmp, 80, 100);
+        //Core.rotate( gray, col, Core.ROTATE_90_CLOCKWISE);
+
+        Imgproc.cvtColor( gray,col, Imgproc.COLOR_GRAY2RGB);
+       // Core.rotate( col.t(), col, 2);
+        //Core.transpose(col,col);
+
+
+        cascadeClassifier = new CascadeClassifier(initAssetFile("haarcascade_frontalface_default.xml"));
+        Log.i(TAG,"cascadeClassifier open: "+cascadeClassifier.empty());
+        MatOfRect faces=new MatOfRect();
+        if (cascadeClassifier != null) {
+            cascadeClassifier.detectMultiScale(gray, faces, 1.1, 1, 1,
+                    new Size(absoluteFaceSize, absoluteFaceSize), new Size());
+            Log.i(TAG,"inside the if");
+        }
+
+
+
+        Rect[] facesArray = faces.toArray();
+        Log.i(TAG,"facesArray length : "+facesArray.length);
+        for (int i = 0; i < facesArray.length; i++) {
+            //Drawing Functions on the IMGPROC
+            // https://docs.opencv.org/3.0-beta/modules/core/doc/drawing_functions.html
+            Imgproc.rectangle(col, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);
+            Imgproc.circle(col,new Point(facesArray[i].x+(facesArray[i].width/2),facesArray[i].y+(facesArray[i].width/2)),facesArray[i].height/8,new Scalar(255, 0, 0, 0),-1);
+            Log.i(TAG,"inside the for");
+        }
+
 
         return col;
     }
